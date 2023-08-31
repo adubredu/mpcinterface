ad105dd9e5364458c836cf27fece5b9f1e084a7c
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2023-08-30 21:23:01 -0400

    set up header file

--- include/mpcinterface.h
+++ include/mpcinterface.h
@@ -1,1 +1,84 @@
 #include <Eigen/Core>
+
+class MPCInterface
+{
+public:
+    // Constructor
+    // Inputs:
+    // Horizon length H (int) 
+    // State dimension n (int)
+    // Input dimension m (int)
+    MPCInterface(int H, int n, int m);
+
+    // Destructor
+    ~MPCInterface();
+
+    // Inputs:
+    // State cost matrix
+    Eigen::VectorXd Q;
+
+    // Input cost matrix
+    Eigen::VectorXd R;
+
+    // Terminal state cost matrix
+    Eigen::VectorXd P;
+
+    // State lower bound
+    Eigen::VectorXd x_min;
+
+    // State upper bound
+    Eigen::VectorXd x_max;
+
+    // Input lower bound
+    Eigen::VectorXd u_min;
+
+    // Input upper bound
+    Eigen::VectorXd u_max;
+
+    // State dynamics matrix
+    Eigen::MatrixXd A;
+
+    // Input dynamics matrix
+    Eigen::MatrixXd B;
+    
+    // State reference
+    Eigen::VectorXd x_ref;
+
+    // Input reference
+    Eigen::VectorXd u_ref;
+
+    // Outputs:
+    struct CanonicalForm {
+        // Objective Matrix
+        Eigen::MatrixXd P_bar;
+
+        // Objective Vector
+        Eigen::VectorXd q_bar;
+
+        // Constraint Matrix
+        Eigen::MatrixXd A_bar;
+
+        // Lower bound vector
+        Eigen::VectorXd l_bar;
+
+        // Upper bound vector
+        Eigen::VectorXd u_bar;
+    };
+
+    // Get the canonical form of the MPC problem
+    CanonicalForm getCanonicalForm();
+
+private:
+    // Horizon length
+    int H;
+
+    // State dimension
+    int n;
+
+    // Input dimension
+    int m;
+
+
+
+
+};
--- src/mpcinterface.cpp
+++ src/mpcinterface.cpp
@@ -1,1 +1,51 @@
 #include "mpcinterface.h"
+
+MPCInterface::MPCInterface (int H, int n, int m) {
+    // Initialize horizon length
+    this->H = H;
+
+    // Initialize state dimension
+    this->n = n;
+
+    // Initialize input dimension
+    this->m = m;
+
+    // Initialize state cost matrix
+    this->Q = Eigen::VectorXd::Zero(n, n);
+
+    // Initialize input cost matrix
+    this->R = Eigen::VectorXd::Zero(m, m);
+
+    // Initialize terminal state cost matrix
+    this->P = Eigen::VectorXd::Zero(n, n);
+
+    // Initialize state lower bound
+    this->x_min = Eigen::VectorXd::Zero(n);
+
+    // Initialize state upper bound
+    this->x_max = Eigen::VectorXd::Zero(n);
+
+    // Initialize input lower bound
+    this->u_min = Eigen::VectorXd::Zero(m);
+
+    // Initialize input upper bound
+    this->u_max = Eigen::VectorXd::Zero(m);
+
+    // Initialize state dynamics matrix
+    this->A = Eigen::MatrixXd::Zero(n, n);
+
+    // Initialize input dynamics matrix
+    this->B = Eigen::MatrixXd::Zero(n, m);
+
+    // Initialize state reference
+    this->x_ref = Eigen::VectorXd::Zero(n);
+
+    // Initialize input reference
+    this->u_ref = Eigen::VectorXd::Zero(m);
+}
+
+MPCInterface::~MPCInterface() {}
+
+MPCInterface::CanonicalForm MPCInterface::getCanonicalForm() {
+    
+}
